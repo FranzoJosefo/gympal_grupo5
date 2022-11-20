5f11ec2a252a6b3a0ba94b79d746dfd1e3ba9c30
Francisco Olivero <froliven@amazon.com>
2022-11-20 20:51:08 -0300

    Added IAuthAdapter, Credentials and GoogleAuthAdapter

--- src/main/java/edu/uade/controllers/LoginController.java
+++ src/main/java/edu/uade/controllers/LoginController.java
@@ -1,12 +1,19 @@
 package edu.uade.controllers;
 
+import edu.uade.auth.Credentials;
+import edu.uade.auth.GoogleAuthAdapter;
 import edu.uade.auth.IAuthAdapter;
+import edu.uade.auth.Result;
 
 public class LoginController {
 
-    private IAuthAdapter authAdapter;
+    private final IAuthAdapter authAdapter = new GoogleAuthAdapter();
 
-    public Boolean doLogin() {
-        return true;
+    public Result doLogin(Credentials creds) {
+        return authAdapter.doLogin(creds);
+    }
+
+    public Result registerUser(Credentials creds) {
+        return authAdapter.registerUser(creds);
     }
 }
