4517630de0851108b2c29a7c7db801d697d584f6
Francisco Olivero <froliven@amazon.com>
2022-11-21 01:38:23 -0300

    Rearanged packages

--- src/main/java/edu/uade/backend/app/controllers/LoginController.java
+++ src/main/java/edu/uade/backend/app/controllers/LoginController.java
@@ -4,10 +4,16 @@ import edu.uade.backend.app.model.Credentials;
 import edu.uade.backend.app.auth.GoogleAuthAdapter;
 import edu.uade.backend.app.auth.IAuthAdapter;
 import edu.uade.backend.app.model.enums.Result;
+import edu.uade.backend.base.controllers.BaseController;
+import edu.uade.compartido.mensajeria.CentroDeMensajes;
 
-public class LoginController {
+public class LoginController extends BaseController {
 
     private final IAuthAdapter authAdapter = new GoogleAuthAdapter();
+
+    public LoginController(CentroDeMensajes centroDeMensajes) {
+        super(centroDeMensajes);
+    }
 
     public Result doLogin(Credentials creds) {
         return authAdapter.doLogin(creds);
